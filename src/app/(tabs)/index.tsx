77cac792fd84f947e605bc8ca663b6d5c0bd6286
Roger Rosa <rogeritael.contato@gmail.com>
2024-09-07 11:10:39 -0300

    component: Input de pesquisa

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
+import { Input } from '@/components/input'
 import { Text, View } from 'react-native'
 
 export default function Home(){
     return (
-        <View className="flex-1 bg-gray-900">
-            <Text className="">oi</Text>
+        <View className="flex-1 bg-gray-900 pt-14 p-4">
+            <Input>
+                <Input.Field placeholder='pesquisar no e-mail...' />
+            </Input>
         </View>
     )
 }
